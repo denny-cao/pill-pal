dea947ca3fb582f614ef64efe3f019506b8df175
ameshabanjara <abanjara@stanford.edu>
2024-02-17 12:49:31 -0800

    Initial Commit

---
+++ PillPal/PillPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillPalApp.swift
+//  PillPal
+//
+//  Created by Amesha Banjara on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PillPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PillPalUITests/PillPalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PillPalUITestsLaunchTests.swift
+//  PillPalUITests
+//
+//  Created by Amesha Banjara on 2/17/24.
+//
+
+import XCTest
+
+final class PillPalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
