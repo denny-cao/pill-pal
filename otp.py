569891a55d5c5e486561c688cfe4d21f0d8af46d
Denny Cao <dennycaopersonal@gmail.com>
2024-02-17 16:23:37 -0800

    add post requests

---
+++ otp.py
@@ -0,0 +1,48 @@
+from twilio.rest import Client
+import random
+import threading
+import time
+import dotenv
+
+client = Client()
+dotenv.load_dotenv()
+account_sid = dotenv.get('TWILIO_ACCOUNT_SID')
+auth_token = dotenv.get('TWILIO_AUTH_TOKEN')
+
+class Otp:
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+        self.otp = random.randint(1000, 9999)
+        self.expiration_time = 120
+        self.expired = False
+        self.timer = threading.Thread(target=self.expire_otp)
+        self.timer.start()
+
+    def expire_otp(self):
+        time.sleep(self.expiration_time)
+        self.expired = True
+
+    def send_otp(self):
+        message = client.messages.create(
+            body=f"Your OTP is: {self.otp}",
+            from_="YOUR_TWILIO_NUMBER",
+            to=self.phone_number
+        )
+        print(message.sid)
+
+    def verify_otp(self, user_otp):
+        if self.expired:
+            return False
+        if int(user_otp) == self.otp:
+            return True
+        return False
+
+    def resend_otp(self):
+        self.otp = random.randint(1000, 9999)
+        self.expired = False
+        self.timer = threading.Thread(target=self.expire_otp)
+        self.timer.start()
+        self.send_otp()
+
+    def change_phone_number(self, phone_number):
+        self.phone_number = phone_number
--- pillpal.py
+++ pillpal.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import psycopg2
-import time
 import os
+import requests
+import json
+import datetime
+# import OTP as otp
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,4 +15,55 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
+@app.post('/api/new-patient')
+def new_patient_otp():
+    data = request.get_json()
+    name = data['name']
+    number = data['phone_number']
+    if not cur.execute("SELECT * FROM patients WHERE phone_number = %s", (number,)):
+        cur.execute("INSERT INTO patients (name, phone_number) VALUES (%s, %s)", (name, number))
+        return "Patient added", 200
+    else:
+        return "Patient already exists", 400
 
+#     # Validate phone phone_number (10 digits)
+#     if len(number) != 10:
+#         return "Invalid phone number", 400
+#     elif not number.isdigit():
+#         return "Invalid phone number", 400
+# 
+#     # Check if patient already exists
+#     if cur.execute("SELECT * FROM patients WHERE phone_number = %s", (name, number)):
+#         return "Patient already exists", 400
+# 
+#     # OTP generation
+#     otp = otp(number)
+# 
+#     otp.send_otp()
+# 
+#     return "OTP sent", 200
+
+# @app.post('/api/verify-patient-otp')
+# def verify_patient_otp():
+#     data = request.get_json()
+#     user_otp = data['otp']
+# 
+#     if otp.verify_otp(user_otp):
+#         return "OTP verified", 200
+#     else:
+#         return "Invalid OTP", 400
+
+@app.post('/api/new-caretaker')
+def new_caretaker():
+    data = request.get_json()
+    patient_id = data['patient_id']
+    with conn:
+        # Check if patient exists
+        patients = cur.execute("SELECT * FROM patients WHERE id = %s", (patient_id,))
+
+        if patients:
+            caretaker_name = data['name']
+            cur.execute("INSERT INTO caretakers (name, patient_id) VALUES (%s, %s)", (caretaker_name, patient_id))
+            return "Caretaker added", 200
+        else:
+            return "Patient does not exist", 400
