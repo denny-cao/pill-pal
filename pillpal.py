3698526b49310abb3dd1258aecece5561fc3a745
Denny Cao <dennycaopersonal@gmail.com>
2024-02-17 19:44:15 -0800

    attempt to add otp

--- pillpal.py
+++ pillpal.py
@@ -5,7 +5,7 @@ import os
 import requests
 import json
 import datetime
-# import OTP as otp
+import otp
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,11 +20,35 @@ def new_patient():
     data = request.get_json()
     name = data['name']
     number = data['phone_number']
-    if not cur.execute("SELECT * FROM patients WHERE phone_number = %s", (number,)):
+    # Validate phone number (10 digits)
+    if len(number) != 10 or number.isdigit():
+        return "Invalid phone number", 400
+    elif cur.execute("SELECT * FROM patients WHERE phone_number = %s", (number,)):
+        return "Patient already exists", 400
+
+    otp_instance = otp.OTP(number)
+    otp_instance.send_otp()
+    return "OTP sent", 200
+
+@app.post('/api/verify-patient-otp')
+def vertify_patient_otp():
+    data = request.get_json()
+    name = data['name']
+    number = data['phone_number']
+    user_otp = data['otp']
+
+    if otp.verify_otp(user_otp):
         cur.execute("INSERT INTO patients (name, phone_number) VALUES (%s, %s)", (name, number))
         return "Patient added", 200
     else:
-        return "Patient already exists", 400
+        return "Invalid OTP", 400
+
+# 
+#     if not cur.execute("SELECT * FROM patients WHERE phone_number = %s", (number,)):
+#         cur.execute("INSERT INTO patients (name, phone_number) VALUES (%s, %s)", (name, number))
+#         return "Patient added", 200
+#     else:
+#         return "Patient already exists", 400
 
 #     # Validate phone phone_number (10 digits)
 #     if len(number) != 10:
@@ -58,12 +82,18 @@ def new_caretaker():
     data = request.get_json()
     patient_id = data['patient_id']
     with conn:
-        # Check if patient exists
-        patients = cur.execute("SELECT * FROM patients WHERE id = %s", (patient_id,))
-
-        if patients:
+        if cur.execute("SELECT * FROM patients WHERE id = %s", (patient_id,)):
             caretaker_name = data['name']
             cur.execute("INSERT INTO caretakers (name, patient_id) VALUES (%s, %s)", (caretaker_name, patient_id))
             return "Caretaker added", 200
         else:
             return "Patient does not exist", 400
+
+@app.post('/update-next-dose')
+def update_next_dose():
+    data = request.get_json()
+    medication_id = data['medication_id']
+    next_dose = data['next_dose']
+    with conn:
+        cur.execute("UPDATE medications SET next_dose = %s WHERE id = %s", (next_dose, medication_id))
+        return "Next dose updated", 200
