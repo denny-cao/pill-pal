2ecc2ffcf93fb0ba900c00657c40b0e5d1bf7ca0
ameshabanjara <abanjara@stanford.edu>
2024-02-17 22:37:27 -0800

    patient portal final steps, unique id

--- PillPal/ContentView.swift
+++ PillPal/ContentView.swift
@@ -8,18 +8,63 @@
 import SwiftUI
 
 struct PatientView: View {
+    @State private var name: String = ""
+    @State private var phoneNumber: String = ""
+    
     var body: some View {
-        Text("Welcome to your PillPal Patient Portal!")
-        
-            .navigationTitle("Patient Portal")
+        VStack {
+            Text("Welcome to your PillPal Patient Portal!")
+                .font(.title2)
+                .padding()
+            
+            TextField("Please enter your name here...", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Please enter your phone number here...", text: $phoneNumber)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .onChange(of: phoneNumber) { newValue in
+                    let filtered = newValue.filter { $0.isNumber }
+                    if filtered != newValue {
+                        self.phoneNumber = filtered
+                    }
+                }
+            
+            NavigationLink(destination: UniqueID()) {
+                Text("Submit")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+        }
+        .navigationTitle("Patient Portal")
     }
 }
 
 struct CaregiverView: View {
     var body: some View {
         Text("Caregiver View")
-        Text("Enter your number and name por favor")
             .navigationTitle("Caregiver Portal")
+    }
+}
+
+struct UniqueID: View {
+    var randomCode: String {
+        let digits = "123456789"
+        let code = String((0..<6).map{ _ in digits.randomElement()! })
+        return code
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Thank you for your information!")
+            Text("Your PillPal ID:  # \(randomCode)")
+                .font(.title)
+        }
+        .navigationTitle("PillPal Unique ID")
     }
 }
 
@@ -30,11 +75,11 @@ struct ContentView: View {
                 Text("Welcome to...")
                 Text("Pill Pal!")
                     .font(.headline)
-                    //.padding()
+                
                 Image(systemName: "pill")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
                 NavigationLink(destination: PatientView()) {
                     Text("I'm a Patient!")
                         .padding()
